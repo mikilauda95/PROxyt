b49f2da9162a26668aeadc27c2e4c462907f5c51
Michele Simili <msimili@qti.qualcomm.com>
2022-03-29 19:39:40 +0100

    PseudoCode + Function to read csv with card name and number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys,os
+import csv
 import time
 import argparse
 
@@ -18,6 +19,16 @@ def doArgs(argList, name):
     parser.add_argument('--output', action="store", dest="outputFn", type=str, help="Output file name", required=True)
 
     return parser.parse_args(argList)
+
+def ReadCardsFromCSV(file_name):
+    c_list = []
+    fd = open(file_name)
+    csvreader = csv.reader(fd)
+
+    for c in csvreader:
+        c_list.append(c)
+    print(c_list)
+    return c_list
 
 def main():
     progName = "Template"
@@ -39,6 +50,13 @@ def main():
         print("Output directory doesn't exist, making output dirs: %s" % (outputBase))
         os.makedirs(outputBase)
 
+    # Read Cards name from file and put them in a list for elaboration
+
+    card_list = ReadCardsFromCSV(inputFn)
+
+    # Search the image for each card in the list and put it in folder cards
+
+    # Create PDF with all cards repeated by number
 
     print("Finished in %0.4f seconds" % (time.time() - startTime))
     return
